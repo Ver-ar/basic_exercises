effedfb0520f93ebf1b50a89f64b8e3f96540b88
Ver-ar <94222049+Ver-ar@users.noreply.github.com>
2021-12-10 01:31:35 +0300

    Question

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+print("Задание №1\n")
 # Задание 1
 # Дан список учеников, нужно посчитать количество повторений каждого имени ученика
 # Пример вывода:
@@ -12,8 +15,19 @@ students = [
     {'first_name': 'Маша'},
     {'first_name': 'Петя'},
 ]
-# ???
+names = []
+for people in students:
+    names.append(people['first_name'])
 
+count = Counter(names)
+counter_name_1 = count['Вася']
+counter_name_2 = count['Маша']
+counter_name_3 = count['Петя']
+
+print(f"Вася: {counter_name_1}\nМаша: {counter_name_2}\nПетя: {counter_name_3}")
+
+
+print("______________________\n\nЗадание №2\n")
 
 # Задание 2
 # Дан список учеников, нужно вывести самое часто повторящееся имя
@@ -26,8 +40,21 @@ students = [
     {'first_name': 'Маша'},
     {'first_name': 'Оля'},
 ]
-# ???
+names = []
+for people in students:
+    names.append(people['first_name'])
 
+names_str=','.join(names)
+
+words = names_str.split(',')
+
+most_use_name = Counter(words).most_common(1)
+for name in most_use_name:
+    use_name = name[0]
+print(f"Самое частое имя среди учеников: {use_name}")
+
+
+print("______________________\n\nЗадание №3\n")
 
 # Задание 3
 # Есть список учеников в нескольких классах, нужно вывести самое частое имя в каждом классе.
@@ -51,7 +78,26 @@ school_students = [
         {'first_name': 'Саша'},
     ],
 ]
-# ???
+
+names = []
+a=0
+def find_name_in_class():
+    names = []
+    for students in school_class:
+        names.append(students['first_name'])
+        names_str=','.join(names)
+        words = names_str.split(',')
+        most_use_name = Counter(words).most_common(1)
+        for name in most_use_name:
+            use_name = name[0]
+    return (use_name)
+    
+for school_class in school_students:
+    a+=1
+    c = find_name_in_class()
+    print(f"Самое частое имя в классе {a}: {c}")
+
+print("______________________\n\nЗадание №4\n")
 
 
 # Задание 4
@@ -72,7 +118,12 @@ is_male = {
     'Миша': True,
     'Даша': False,
 }
-# ???
+
+for name in school:
+    if is_male[name]:
+        print (f"{name}, пол: мужской")
+    else:
+        print (f"{name}, пол: женский")
 
 
 # Задание 5
--- string_challenges.py
+++ string_challenges.py
@@ -1,28 +1,57 @@
+print("Задание №1\n")
 # Вывести последнюю букву в слове
 word = 'Архангельск'
-# ???
+print (word[-1])
+
+print("______________________\n\nЗадание №2\n")
 
 
 # Вывести количество букв "а" в слове
 word = 'Архангельск'
-# ???
+print (len(word))
 
-
+print("______________________\n\nЗадание №3\n")
 # Вывести количество гласных букв в слове
 word = 'Архангельск'
-# ???
+vowels = 'Ааиеёоуыэюя'
 
+word_2 = word.lower()
+count = 0
+string=[]
+for letter in vowels:
+    if letter in word:
+        string.append(letter)
+count = len(string)
 
+print (count)
+
+print("______________________\n\nЗадание №4\n")
 # Вывести количество слов в предложении
 sentence = 'Мы приехали в гости'
-# ???
+count = len(sentence.split())
 
+print(count)
 
+print("______________________\n\nЗадание №5\n")
 # Вывести первую букву каждого слова на отдельной строке
 sentence = 'Мы приехали в гости'
-# ???
+string_sentence = sentence.split()
+for word in string_sentence:
+    print(word[0])
 
 
+print("______________________\n\nЗадание №6\n")
 # Вывести усреднённую длину слова в предложении
 sentence = 'Мы приехали в гости'
-# ???
+string_sentence = sentence.split()
+count_word = len(sentence.split())
+
+        
+sum_count=0    
+for word in string_sentence:
+    count_words=len(word)
+    sum_count+=count_words
+sum=sum_count/count_word
+sum=int(sum)
+    
+print(f"Усреднённая длина слова в предложении: {sum}")
